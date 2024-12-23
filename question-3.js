a50e0155758919b221f9c72cc72694485f8a6634
Kong Thiraphat <thiraphat.cto@gmail.com>
2024-12-23 10:52:50 +0700

    feat: add answer to exercise

--- question-3.js
+++ question-3.js
@@ -1,20 +1,21 @@
 /*
 What is the Big O notation for Function findStudentById . Explain your reason.
-Answer:
+Answer: Big O Notation ของ Function นี้คือ O(n) เพราะ ต้องมีการวน loop ผ่านสมาชิกทุกตัวใน array 
 
 
 */
 
 /* 
 What is the Big O notation for Function findProductPrice. Explain your reason.
-Answer:
+Answer: Big O Notation ของ Function นี้คือ O(log n) เพราะ มีการลดขนาดของ input ลงทีละครึ่ง
 
 
 */
 
 /* 
 Which function is more efficient and why?
-Answer:
+Answer: function ที่มีประสิทธิภาพมากกว่าคือ findProductPrice เพราะ binary search มี Big O Notation คือ O(log n) ซึ่งมี Time Complexity ดีกว่าเมื่อเทียบกับ O(n) ของ linear search
+
 
 
 */
